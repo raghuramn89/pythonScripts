9fe3aa89f7597af431f2a368c496c6141a0b021f
raghuramn89 <raghuramn89@gmail.com>
2021-10-09 02:08:21 -0400

    get all verbose details of all the dl

--- getAllAccounts.sh
+++ getAllAccounts.sh
@@ -1,2 +1,4 @@
 # Get all the accounts of Zimbra and print Firstname, Lastname.
 for i in `zmprov -l gaa`; zmprov -l ga $i givenName sn; done; 
+# Display all the distribution lists along with verbose information.
+zmprov -l gadl -v
